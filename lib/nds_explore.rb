8ff9e48882a9722b5e35f062add026a1e8fcbec6
Tanvir Walia <tarywalia@gmail.com>
2020-02-25 22:53:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,4 +10,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  ss_movies = directors_database[0][:movies]
+  index = 0
+
+  while index < ss_movies.length do
+   titles = ss_movies[index][:title]
+   puts titles
+   index +=1
+ end
 end
